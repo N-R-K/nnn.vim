48ece31fda387a6a1463f5c194c053098f282feb
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-10-20 09:31:23 -0300

    Create SIEM.py

---
+++ SIEM.py
@@ -0,0 +1,82 @@
+Situation Overview
+
+A security information event management (SIEM) 
+solution is like a radar system that pilots and air traffic 
+controllers use. Without one, enterprise IT is flying blind. 
+Although security appliances and system software 
+are good at catching and logging isolated attacks and 
+anomalous behavior, today’s most serious threats are 
+distributed, acting in concert across multiple systems 
+and using advanced evasion techniques to avoid 
+detection. Without a SIEM, attacks are allowed to 
+germinate and grow into catastrophic incidents.
+The importance of a SIEM solution to today’s enterprise 
+is magnified by the growing sophistication of attacks 
+and the use of cloud services that only increase the 
+surface of vulnerability. As more businesses evolve 
+their infrastructure into the cloud, security strategies 
+and technology must also adapt. Organizations 
+increasingly are turning to SIEM deployed in the cloud 
+or as software-as-a-service (SaaS) to strengthen 
+security management while reducing the resources 
+required for protection.
+SIEM: What’s Inside?
+Enterprise security teams must use a SIEM solution 
+that not only solves common security use cases, but 
+also advanced use cases as well. To keep up with the 
+dynamic threat landscape, modern SIEMs are expected 
+to be able to:
+• Centralize and aggregate all security-relevant 
+events as they’re generated from their source
+• Support a variety of reception, collection 
+mechanisms including syslog, file transmissions, 
+file collections and more
+• Add context and threat intelligence to 
+security events 
+• Group correlation searches into clusters of 
+events to enhance visibility and responsiveness 
+with focused threat detection and accelerated 
+incident investigation
+• Correlate and alert across a range of data 
+• Detect advanced and unknown threats 
+• Profile behavior across the organization
+• Ingest all data (users, applications) and make 
+them available for use — monitoring, alerting, 
+investigation and ad hoc searching
+• Reduces risk by enabling faster detection and 
+incident response to newly discovered and ongoing 
+threats with ready to use relevant content
+• Provide ad hoc searching and reporting from data 
+for advanced breach analysis and track potential 
+attackers’ actions
+• Investigate incidents and conduct forensic 
+investigations for detailed incident analysis
+• Assess and report on compliance posture 
+• Use analytics and report on security posture
+Although primarily gathered from servers and network 
+device logs, SIEM data also can come from endpoint 
+security, network security devices, applications, cloud 
+services, authentication and authorization systems and 
+online databases of existing vulnerabilities and threats. 
+But data aggregation is only half of the story. SIEM 
+software then correlates the resulting repository 
+looking for unusual behavior, system anomalies and 
+other indicators of a security incident. This information 
+is used not only for real-time event notification, but 
+also for compliance audits and reporting, performance 
+dashboards, historical trend analysis and post-hoc 
+incident forensics.
+Given the escalating number and sophistication of 
+security threats, along with the increasing value of 
+digital assets in every organization, it’s not surprising 
+that adoption of SIEM continues to grow as part of 
+the overall IT security ecosystem. Gartner pegged the 
+growth for the SIEM market at 12.5 percent in 2015 and 
+expects the double-digit growth to continue in 2016 
+and beyond.1
+A SIEM solution is like a radar system that 
+pilots and air traffic controllers use. Without 
+one, IT is flying blind.
+
+1. “Magic Quadrant for Security Information and Event Management,” by Kelly Kavanagh, Oliver Rochford, Gartner, July 20, 2015, 
+ID G00267505, 
